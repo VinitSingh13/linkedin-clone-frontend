064eaab64ec2ae897d70425a1b9d1eb75aff4af3
“VinitSingh13” <vinitsingh270@gmail.com>
2023-08-22 14:54:23 +0530

    src change

--- src/components/UserImage.jsx
+++ src/components/UserImage.jsx
@@ -8,9 +8,7 @@ const UserImage = ({ image, size = "60px" }) => {
         width={size}
         height={size}
         alt="user"
-        src={`https://my-linkedin-clone-backend.onrender.com
-
-        /assets/${image}`}
+        src={`https://my-linkedin-clone-backend.onrender.com/assets/${image}`}
       />
     </Box>
   );
